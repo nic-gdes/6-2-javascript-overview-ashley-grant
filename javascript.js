cf28002e36df804d24f64e517b258c5984f806c0
Ashley Grant <ashleygrant@Ashleys-MBP-3.tds.net>
2023-10-06 20:35:05 -0700

    Finished 1.1-1.7

--- javascript.js
+++ javascript.js
@@ -1,13 +1,67 @@
 // 1.1 Assignment: Where do you put JavaScript?
 
+// I put the link at the top of the body and it looks like this: <link rel="stylesheet" href="javascript.js">
+alert("Java script is easy");
+//The alert popped up when I put it in the console
+
 // 1.2 Assignment: Variables
+
+var firstName = "John";
+var age = "25";
+var gender = "male";
+var placeOfOrigin = "United Kingdom";
+
+//I created all of the different variables about John with the information i had provided and then put all of it in the console with console.logs after to test every variable.
+
+console.log(firstName);
+console.log(age);
+console.log(gender);
+console.log(placecOfOrigin);
 
 // 1.3 Assignment: Variable Declaration & Initialization
 
+var a = 4;
+var b = "Cool Stuff";
+var c = null;
+var d = false;
+var e;
+
+//I put all of the varibles into the console and console.logged each of them and they came out displaying the same value that I assigned them.
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+
 // 1.4 Assignment: Complete your statements
+
+x = 2;
+y = 3;
+z = x + y;
+
+//I put each statement on a separate line and ended it with a semicolin so the console will read it as a full and complete statement with no confusion.
 
 // 1.5 Assignment: Brackets, Braces and Parentheses
 
+var a = function(){
+    alert('Statement 1');
+    alert('Statement 2');
+    }
+
+//I typed the original code in the console with the brackets and then changed it to curly braces because it gave me an error and then the error disappeared.
+
 // 1.6 Assignment: Data Types
 
+var n = "Ashley Grant"; //double quote string
+var a = 19; //number
+var c = true; //boolean
+
 // 1.7 Assignment: Practicing JavaScript basics
+
+var a = 4;
+var b = 3;
+var c = 5;
+console.log(a * b / 2);
+
+//I gave the correct value to the base, height, and hypotenuse. The console.log preformed the calculation and ended with 6.
